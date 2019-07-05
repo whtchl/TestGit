a2f745bb03aa75cd7a8a79d45303441bc8cf3c4e
whtchl <whtchl@126.com>
2019-07-05 11:13:20 +0800

    dd

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("tchl","this is dev Branch");
+        hh
     }
 }
