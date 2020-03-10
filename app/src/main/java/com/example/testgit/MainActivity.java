b03990db8b385837e92cf70e39e715480749fed9
taochenli <taochenli@supcon.com>
2020-03-10 09:30:15 +0800

    tag1.00.03

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("tchl","this is dev Branch");
-        hh
     }
 }
